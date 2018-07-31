a7b0c2c00212ba7e7173844ac585932b74ad9efe
bpost <bpost@paychex.com>
2018-07-31 13:36:16 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//import { SearchFormComponent } from './search-form/search-form.component';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'smart-rec-ui';
+  title = 'Product Recommendation';
+  selectedClient: string;
+  searchResults: string;
+  searchForm: FormGroup;
+
+  ngOnInit() {
+    this.searchForm = new FormGroup({
+      searchString: new FormControl(
+          '',[Validators.required]
+          // [
+          //     Validators.required,
+          //     Validators.pattern('^[0-9]*$')
+          // ]
+      )
+    });
+  }
+
+  search() {
+    this.searchResults = this.searchForm.controls['searchString'].value;
+  }
 }
---
+++ src/app/search-form/search-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-form',
+  templateUrl: './search-form.component.html',
+  styleUrls: ['./search-form.component.css']
+})
+export class SearchFormComponent implements OnInit {
+  searchData = new FormControl('');
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
